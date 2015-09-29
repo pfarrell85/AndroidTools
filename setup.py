f92c0fd059069b2279d01f6f8f8267330048f896
Patrick Farrell <pfarrell@persistentsystems.com>
2015-09-29 12:28:00 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'aadb',
+  packages = ['aadb'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A tool to manage multiple Android devices connected to a host using Android Debug Bridge (adb)',
+  author = 'Patrick Farrell',
+  author_email = 'pfarrell85@gmail.com',
+  url = 'https://github.com/pfarrell85/AndroidTools', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['Android', 'adb'], # arbitrary keywords
+  classifiers = [],
+)
